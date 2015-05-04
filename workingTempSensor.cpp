5ba8d771568f379b24637354f493750ba974e107
Vinay Londhe <vlondhe@seas.upenn.edu>
2015-05-04 16:47:40 -0400

    Removed merge conflicts from workingTempSensor.cpp

--- workingTempSensor.cpp
+++ workingTempSensor.cpp
@@ -34,12 +34,6 @@ void signal_handler(int signum)
 
 void program_init(){
     char *bus = "/dev/i2c-1"; /* Pins P9_17 and P9_18 */
-<<<<<<< HEAD
-=======
-    //int addr = MCP9808_ADDR; /* The I2C address of TMP102 */
-    //char bufRead[BUFF_READ_SIZE] = {0};
-    //char bufWrite[BUFF_WRITE_SIZE] = {0};
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
 
     if ((fd = open(bus, O_RDWR)) < 0) {
         /* ERROR HANDLING: you can check errno to see what went wrong */
@@ -56,10 +50,7 @@ void program_init(){
     /* Register the signal handler */
     signal(SIGINT, signal_handler);
     
-<<<<<<< HEAD
     // Write to I2C bus
-=======
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
     bufWrite[0] = MCP9808_REG_CONF;
     bufWrite[1] = 0x00;
     bufWrite[2] = 0x00;
@@ -72,27 +63,17 @@ void program_init(){
 string runI2C(){
     char TempCelsiusDisplay[] = "+abc.dd C";
     int tempval;
-<<<<<<< HEAD
     // Write temp register on i2c bus
-=======
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
     bufWrite[0] = MCP9808_REG_TEMP;
     if(write(fd, bufWrite, 1) !=1){
         perror("Failed to Reset Address in readFullSensorState()\n ");
     }
-<<<<<<< HEAD
     // Using I2C Read
-=======
-        // Using I2C Read
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
     if (read(fd,bufRead,BUFF_READ_SIZE) != BUFF_READ_SIZE) {
          /* ERROR HANDLING: i2c transaction failed */
          perror("Failed to read from the i2c bus.\n");
     }
-<<<<<<< HEAD
     // Performs the required conversion of data to celsius after reading from i2c bus
-=======
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
     else {
 
          if(bufRead[0] & 0xE0) {
@@ -104,7 +85,6 @@ string runI2C(){
             TempCelsiusDisplay[0] = '-';
             tempval = 256 - (bufRead[0] << 4) + (bufRead[1] >> 4);
         } else {
-<<<<<<< HEAD
             TempCelsiusDisplay[0] = '+';
             tempval = (bufRead[0] << 4) + (bufRead[1] >> 4);
         }
@@ -132,48 +112,9 @@ string runI2C(){
         TempCelsiusDisplay[3] = ((tempval % 100) % 10) + 0x30;
         string tempDisplay = string(TempCelsiusDisplay);
  
-	    return tempDisplay;
+        return tempDisplay;
     
     }
 
-=======
-        TempCelsiusDisplay[0] = '+';
-        tempval = (bufRead[0] << 4) + (bufRead[1] >> 4);
-        }
-                if (bufRead[1] & 0x08) {
-        if(bufRead[1] & 0x04) {
-            TempCelsiusDisplay[5] = '7';
-            TempCelsiusDisplay[6] = '5';
-        } else {
-            TempCelsiusDisplay[5] = '5';
-            TempCelsiusDisplay[6] = '0';
-        }
-    } else {
-        if(bufRead[1] & 0x04) {
-            TempCelsiusDisplay[5] = '2';
-            TempCelsiusDisplay[6] = '5';
-        }else{
-            TempCelsiusDisplay[5] = '0';
-            TempCelsiusDisplay[6] = '0';
-        }
-    }
-
-            TempCelsiusDisplay[1] = (tempval / 100) + 0x30;
-            TempCelsiusDisplay[2] = ((tempval % 100) / 10) + 0x30;
-            TempCelsiusDisplay[3] = ((tempval % 100) % 10) + 0x30;
-            string tempDisplay = string(TempCelsiusDisplay);
-         // Display result
-         //string command = "python displayOnLCD.py ";
-         //command.append(tempDisplay);
-
-         //cout << command << "\r\n";
-           //system(command.c_str());
-          // cout << tempDisplay << "\r\n";
-	   return tempDisplay;
-         //   printf("temp = %s\r\n", tempDisplay);
-//            printf("Temp Fahrenheit: %f Celsius: %f\n", f, c);
-        }
-        sleep(1);
->>>>>>> 6234c5938c94971fd32bb80b5c06603ab3e5b355
     
 }
